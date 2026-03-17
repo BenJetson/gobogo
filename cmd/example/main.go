97f9623b812e998442508d968020c8ba793ef427
Ben Godfrey <BenJetson@users.noreply.github.com>
2026-03-17 17:02:25 -0400

    Clean up a little.

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -10,9 +10,11 @@ import (
 func main() {
 	s := []int{2, 7, 5, 8, 4, 3, 10, 0, 6, 9, 1}
 	fmt.Println("input:", s)
+
 	begin := time.Now()
 	bogo.Sort(s)
 	elapsed := time.Since(begin)
+
 	fmt.Println("output:", s)
-	fmt.Println("elapsed:", elapsed.Seconds(), "seconds")
+	fmt.Println("sorted in", elapsed.Seconds(), "seconds")
 }
